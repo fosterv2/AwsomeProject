81ee5b5975cb29b6c6eb5e5fd8875a71b0b72ef6
fosterv2 <fosterv2@outlook.com>
2021-01-12 14:54:07 -0800

    Testing some stuff

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Some more info</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +15,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    color: 'white',
+    backgroundColor: 'grey',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
 });
